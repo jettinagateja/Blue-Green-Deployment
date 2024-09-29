0b4eddeaa7d9f4232883e058b60971c44589eb54
jettinagateja <58877208+jettinagateja@users.noreply.github.com>
2024-09-29 18:43:37 +0530

    Update SecurityConfig.java

--- src/main/java/com/example/bankapp/config/SecurityConfig.java
+++ src/main/java/com/example/bankapp/config/SecurityConfig.java
@@ -1,5 +1,5 @@
 package com.example.bankapp.config;
-
+ 
 import com.example.bankapp.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
