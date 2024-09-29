c675c57d9fde936512e7f4acc0b44270b41d053d
jettinagateja <58877208+jettinagateja@users.noreply.github.com>
2024-09-29 18:44:39 +0530

    Update BankappApplication.java

--- src/main/java/com/example/bankapp/BankappApplication.java
+++ src/main/java/com/example/bankapp/BankappApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BankappApplication {
+public class BankappApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankappApplication.class, args);
