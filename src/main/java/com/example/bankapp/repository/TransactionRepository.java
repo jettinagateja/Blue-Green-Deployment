6acc1e27e98c5017b27ca8112e1118d8e56d8834
jettinagateja <58877208+jettinagateja@users.noreply.github.com>
2024-09-29 18:44:30 +0530

    Update TransactionRepository.java

--- src/main/java/com/example/bankapp/repository/TransactionRepository.java
+++ src/main/java/com/example/bankapp/repository/TransactionRepository.java
@@ -3,7 +3,7 @@ package com.example.bankapp.repository;
 import com.example.bankapp.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
+import java.util.List; 
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
     List<Transaction> findByAccountId(Long accountId);
